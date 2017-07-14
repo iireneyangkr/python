b218c8ca3c49332f6995e99f7fceade4d9952b78
Irene Yang <Irene@Minsoos-MacBook-Air.local>
2017-07-14 10:22:11 +0800

    first commit

---
+++ 01-conversation.py
@@ -0,0 +1,15 @@
+# 화면에 데이터 출력하기
+# print(1 + 2)
+
+# 사용자로 부터 값 받기
+# 변수 활용
+# name = input("당신의 이름은 무엇입니까?")
+# print ("안녕하세요, " + name)
+
+# age = input("당신은 몇 살입니까?")
+# print (age + "살 이시군요")
+
+# print("안녕" + "하세요")
+
+conversation = input("이름이 뭐에요?")
+print(conversation + "님, 정말 아름다우시네요.")
